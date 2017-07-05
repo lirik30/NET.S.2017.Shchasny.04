5128066fcfef42cd74e6b0326df08f3b5a994df9
Kiril Shchasny <kiril3467@yandex.ru>
2017-07-05 21:39:11 +0300

    Add DD tests for FindRootOfNumber method

---
+++ NumericAlgorithms.Tests/NAlgorithmsTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static NumericAlgorithms.NAlgorithms;
+
+namespace NumericAlgorithms.Tests
+{
+    [TestClass]
+    public class NAlgorithmsTests
+    {
+        public TestContext TestContext { get; set; }
+
+
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "|DataDirectory|\\PositiveTestVariants.xml",
+            "TestCase",
+            DataAccessMethod.Sequential)]
+        [DeploymentItem("NumericAlgorithms.Tests\\PositiveTestVariants.xml")]
+        [TestMethod]
+        public void FindRootOfNumber_PositiveTests()
+        {
+            double actual = FindRootOfNumber(
+                Convert.ToDouble(TestContext.DataRow["number"]),
+                Convert.ToInt32(TestContext.DataRow["n"])
+            );
+            double expected = Convert.ToDouble(TestContext.DataRow["expectedResult"]);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "|DataDirectory|\\ThrowsArgumentOutOfRangeExceptionTestVariants.xml",
+            "TestCase",
+            DataAccessMethod.Sequential)]
+        [DeploymentItem("NumericAlgorithms.Tests\\ThrowsArgumentOutOfRangeExceptionTestVariants.xml")]
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void FindRootOfNumber_ThrowsArgumentOutOfRangeException()
+        {
+            FindRootOfNumber(
+                Convert.ToDouble(TestContext.DataRow["number"]),
+                Convert.ToInt32(TestContext.DataRow["n"])
+            );
+        }
+
+    }
+}
---
+++ NumericAlgorithms.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NumericAlgorithms.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NumericAlgorithms.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f3e7c17c-6217-4a8d-b395-a4ac2c7d9e9d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
